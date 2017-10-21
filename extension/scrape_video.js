f108a5f965b1759e9fa8c360f942388e05b01ece
dannycho7 <dannycho7@gmail.com>
2017-10-20 23:01:30 -0700

    Setup Browserify to include npm modules in scrape_video.js

--- extension/scrape_video.js
+++ extension/scrape_video.js
@@ -1,3 +1,9 @@
+const qs = require("qs");
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-	console.log("received message");
+	console.log(window.location.href);
 });
+
+function getTime() {
+	return document.getElementById("movie_player").getCurrentTime();
+}

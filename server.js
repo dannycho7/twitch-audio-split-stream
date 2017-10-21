782638201a8be253e8fd8d1ad5e676bdc114af44
dannycho7 <dannycho7@gmail.com>
2017-10-20 21:57:22 -0700

    Connect socket.io on server end

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+var server = require("http").Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, "static")));
 
@@ -8,4 +10,11 @@ app.get("/current", (req, res) => {
 	res.json({ ytVideoSrc: "https://www.youtube.com/embed/m7mvpe1fVa4?autoplay=1&start=100"});
 });
 
-app.listen(5000, () => console.log("Server listening in on port 5000"));
+io.on("connection", function (socket) {
+  socket.emit("changeSong", { ytVideoSrc: "https://www.youtube.com/embed/m7mvpe1fVa4?autoplay=1&start=150" });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+server.listen(5000, () => console.log("Server listening in on port 5000"));

d6a8cc03f33135cd3bcae77febdf7f2d7b2e4e7c
dannycho7 <dannycho7@gmail.com>
2017-10-21 00:49:37 -0700

    Scrape video returns the current song with time to requesting background socket

--- extension/src/scrape_video.js
+++ extension/src/scrape_video.js
@@ -4,10 +4,16 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 	let url = parse(window.location.href, true);
 
 	if(url.query["v"]) {
+		if(message["type"] == "current") {
+			console.log("Requested for current song");
+			let embed_url = `https://www.youtube.com/embed/${url.query["v"]}` +
+						`?autoplay=1&start=${getTime() || 0}`;
+			return sendResponse({ ytVideoSrc: embed_url });
+		}
 		let embed_url = `https://www.youtube.com/embed/${url.query["v"]}` +
 						`?autoplay=1&start=${url.query["t"] || 0}`;
 
-		chrome.runtime.sendMessage({ embed_url }, function(response) {
+		chrome.runtime.sendMessage({ ytVideoSrc: embed_url }, function(response) {
 			
 		});
 	}

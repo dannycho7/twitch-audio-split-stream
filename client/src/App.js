62de0584358323f887c4b1882e3348be6bdfc2fc
dannycho7 <dannycho7@gmail.com>
2017-10-20 20:34:44 -0700

    Init Create React App basic

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Player from "./Player";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Player />
+      </div>
+    );
+  }
+}
+
+export default App;

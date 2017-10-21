4a78ceb83f661ed1f7c91955d5b8528c424b376a
dannycho7 <dannycho7@gmail.com>
2017-10-21 00:49:18 -0700

    Connect background page socket to content script for querying current songs

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,9 +1,12 @@
 //Listen for when a Tab changes state
 let latest = "";
+let ytTabId = null;
+
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details){
 	if(details.frameId === 0 && details.url.includes("youtube") && details.url !== latest){
 		console.log(details, latest);
 		latest = details.url;
+		ytTabId = details.tabId;
 		chrome.tabs.sendMessage(details.tabId, { data: details }, function(response) {
 			
 		});
@@ -16,5 +19,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	xhttp.addEventListener("load", () => {});
 	xhttp.open("POST", "http://localhost:5000/change");
 	xhttp.setRequestHeader("Content-Type", "application/json");
-	xhttp.send(JSON.stringify({ "ytVideoSrc": message["embed_url"] }))
+	xhttp.send(JSON.stringify({ "ytVideoSrc": message["ytVideoSrc"] }))
 });
+
+var socket = io('http://localhost:5000');
+socket.on('connect', function(){ socket.emit("set_streamer_socket") });
+socket.on('server_request_current', function(data, cb){
+	console.log("Server received request for current song");
+	chrome.tabs.sendMessage(ytTabId, { type: "current" }, cb);
+});
+socket.on('disconnect', function(){});
